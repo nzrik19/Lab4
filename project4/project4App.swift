4dd73543becd2b6cc59af63f77bbb4a04f9eed8a
NLTU <NLTU@lviv.ua>
2025-10-03 08:45:56 +0300

    Initial Commit

---
+++ project4/project4App.swift
@@ -0,0 +1,17 @@
+//
+//  project4App.swift
+//  project4
+//
+//  Created by ІПЗС-21 on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
